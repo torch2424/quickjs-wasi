8d1c70b27f1a55767cd5490c0739f67ef233382a
Aaron Turner <aaron@aaronthedev.com>
2020-06-16 12:27:12 -0700

    Made some progress on compiling a working version of libregexp

--- compile-libregexp.sh
+++ compile-libregexp.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-emcc libregexp.c -g -s WASM=1 -s SIDE_MODULE=1 -o libregexp.wasm 
+# https://github.com/emscripten-core/emscripten/wiki/WebAssembly-Standalone
+# https://webassembly.github.io/wabt/demo/wasm2wat/
+# https://github.com/emscripten-core/emscripten/issues/4844
+
+# TODO: See the issue, tried all the alternatives. This seems to include everything we need.
+# But, Need to link in the stlib stuff, such as string.c
+# https://stackoverflow.com/questions/6481291/libc-source-location-for-download-or-online-viewing
+
+emcc libregexp.c libunicode.c cutils.c -g -s WASM=1 -s SIDE_MODULE=1 -o libregexp.wasm 
